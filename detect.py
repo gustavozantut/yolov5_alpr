747becbc6ffd5a808a5746f8531b4fbf3ac68688
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2022-12-10 01:37:44 -0300

    setting max number for the new feature

--- detect.py
+++ detect.py
@@ -107,7 +107,7 @@ def run(
         bs = 1  # batch_size
     vid_path, vid_writer = [None] * bs, [None] * bs
 
-    frames_since_last_saved = save_crops_each_n_frames #First frame will always save crops
+    frames_since_last_saved = min(save_crops_each_n_frames, 1000) #First frame will always save crops
 
     # Run inference
     model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
@@ -170,7 +170,6 @@ def run(
                         annotator.box_label(xyxy, label, color=colors(c, True))
                     if save_crop and (frames_since_last_saved%save_crops_each_n_frames==0): #Check is frame is 'savable'
                         save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True) #Save crops from detected frame
-                        print("crop saved")
                 if save_detected_frame: #Save Frame if detected any class
                     save_one_frame(annotator.result(), file=save_dir / 'frames'/ f'{p.stem}.png')
                     
