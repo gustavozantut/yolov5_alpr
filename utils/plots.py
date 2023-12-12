835c6c14026b5a490a308a5456238e7466708fa7
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-12-12 20:01:49 -0300

    shutil not workin for latest tag

--- utils/plots.py
+++ utils/plots.py
@@ -554,7 +554,7 @@ def save_one_box(xyxy, im, file=Path('im.jpg'), gain=1.02, pad=10, square=False,
     crop = im[int(xyxy[0, 1]):int(xyxy[0, 3]), int(xyxy[0, 0]):int(xyxy[0, 2]), ::(1 if BGR else -1)]
     if save:
         file.parent.mkdir(parents=True, exist_ok=True)  # make directory
-        f = str(increment_path(file+"_0").with_suffix('.jpg'))
+        f = str(increment_path(file.name+"_0").with_suffix('.jpg'))
         # cv2.imwrite(f, crop)  # save BGR, https://github.com/ultralytics/yolov5/issues/7007 chroma subsampling issue
         Image.fromarray(crop[..., ::-1]).save(f, quality=95, subsampling=0)  # save RGB
     return crop
