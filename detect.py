f77a53b3a90fee9650675be41886cfc5b2cabaee
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2024-01-14 10:38:39 -0300

    change origins from function

--- detect.py
+++ detect.py
@@ -44,8 +44,9 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-from ultralytics.utils.plotting import Annotator, colors, save_one_box, save_one_frame
+from ultralytics.utils.plotting import Annotator, colors, save_one_box
 
+from utils.plots import save_one_frame
 from models.common import DetectMultiBackend
 from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
 from utils.general import (
